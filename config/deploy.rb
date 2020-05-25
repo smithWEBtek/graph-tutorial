7db092b3091b32573976a79668b6c24ccd949768
github username <github email address>
2020-05-25 14:08:05 -0400

    Comment out  tasks

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,15 +38,15 @@ set :keep_releases, 5
 # Uncomment the following to require manually verifying the host key before first deploy.
 set :ssh_options, verify_host_key: :always
 
-before "deploy:assets:precompile", "deploy:npm_install"
+# before "deploy:assets:precompile", "deploy:npm_install"
 
-namespace :deploy do
-  desc 'Run rake npm:install'
-  task :npm_install do
-    on roles(:web) do
-      within release_path do
-        execute("cd #{release_path} && npm install")
-      end
-    end
-  end
-end
+# namespace :deploy do
+#   desc 'Run rake npm:install'
+#   task :npm_install do
+#     on roles(:web) do
+#       within release_path do
+#         execute("cd #{release_path} && npm install")
+#       end
+#     end
+#   end
+# end
